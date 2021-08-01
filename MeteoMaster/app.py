fd893d5dabb480481e70890e8df2928ae354876e
IshanAnshu <42411265+IshanAnshu@users.noreply.github.com>
2021-08-01 11:46:46 +0530

    MeteoMaster Flask Learning

--- MeteoMaster/app.py
+++ MeteoMaster/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World Ishan from main.py!'
+    return 'Hello World Ishan!'
 
 
 if __name__ == '__main__':
